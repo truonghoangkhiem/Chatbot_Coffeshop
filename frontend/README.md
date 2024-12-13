b78a7e74cfcd03cd3988865722d044a5a4d32926
ez4kacchan <daovuhung3010@gmail.com>
2024-12-13 11:24:04 +0700

    set up environment

---
+++ frontend/README.md
@@ -0,0 +1,13 @@
+## Khởi tạo 
+1. Tải dependencies
+
+   ```bash
+   npm install
+   ```
+
+2. Chạy
+
+   ```bash
+    npx expo start
+   ```
+
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,38 +2,24 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { NativeWindStyleSheet } from "nativewind";
-import { CartProvider } from "../components/navigation/Cartcontext";
-import {RootSiblingParent} from 'react-native-root-siblings";
 
 NativeWindStyleSheet.setOutput({
-    default: "native",
+  default: "native",
 });
 
 export default function RootLayout() {
-    const [fontsLoaded] = useFonts({
-        "Sora-Regular": require("@/assets/fonts/Sora-Regular.ttf"),
-        "Sora-Semibold": require("@/assets/fonts/Sora-Semibold.ttf"),
-        "Sora-Bold": require("@/assets/fonts/Sora-Bold.ttf"),
-    });
+  const [fontsLoaded] = useFonts({
+    "Sora-Regular": require("../assets/fonts/Sora-Regular.ttf"),
+    "Sora-SemiBold": require("../assets/fonts/Sora-SemiBold.ttf"),
+    "Sora-Bold": require("../assets/fonts/Sora-Bold.ttf"),
+  });
 
-    if (!fontsLoaded) { 
-        return undefined;
-    }
-    return (
-        <CartProvider>
-            <RootSiblingParent>
-                <Stack>
-                    <Stack.Screen name="index"
-                        options={{ headerShown: false }}
-                    />
-                    <Stack.Screen name="details"
-                        options={{ headerShown: true }}
-                    />
-                    <Stack.Screen name="(tabs)"             //6.56
-                    options = {{headerShown:false}}
-                    />
-                </Stack>
-            </RootSiblingParent>
-        </CartProvider>
-    );
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false }}
+      />
+    </Stack>
+  );
 }
