3ffb95ed9a7a5da8bbf2917ea0a7b0bcc435de9b
Shigoruya <150207709+Nohenshin@users.noreply.github.com>
2024-12-14 17:28:30 +0700

    Update order.tsx

--- frontend/app/{tabs}/order.tsx
+++ frontend/app/{tabs}/order.tsx
@@ -1,12 +1,12 @@
 //Created 7.24
 import {Text, View } from 'react-native';
 import React from 'react';
-import PageHeader from '@/components/PageHeader';
+//import PageHeader from '@/components/PageHeader';//<PageHeader title="Order" showHeaderRight={false} bgColor='#F9F9F9'/>
 
 const Order = () => {
     return (
     <View>
-    <PageHeader title="Order" showHeaderRight={false} bgColor='#F9F9F9'/>
+
     <Text>Order</Text>
     </View>
     )
