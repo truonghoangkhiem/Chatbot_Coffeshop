d669bfb89f9fd655de4b439c73dc400e46014692
Shigoruya <150207709+Nohenshin@users.noreply.github.com>
2024-12-14 17:28:07 +0700

    Update chatRoom.tsx

--- frontend/app/{tabs}/chatRoom.tsx
+++ frontend/app/{tabs}/chatRoom.tsx
@@ -1,15 +1,14 @@
 //Created 7.24
 import {Text, View } from 'react-native';
 import React from 'react';
-import PageHeader from '@/components/PageHeader';
+//import PageHeader from '@/components/PageHeader';<PageHeader title="Chatbot" showHeaderRight={false} bgColor='#F9F9F9'/>
 
 const ChatRoom = () => {
     return (
     <View>
-        <PageHeader title="Chatbot" showHeaderRight={false} bgColor='#F9F9F9'/>
+        
         <Text>ChatRoom</Text>
     </View>
     )
 }
 export default ChatRoom
-
