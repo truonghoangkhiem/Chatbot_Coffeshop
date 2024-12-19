d2327ccd4ffc1003aae6c12f6ed7aec0f86edcc1
unknown <truongkhiemhoang@gmail.com>
2024-12-20 00:16:35 +0700

    add file agent

---
+++ backend/api/agents/classification_agent.py
@@ -0,0 +1,57 @@
+from openai import OpenAI
+import os
+import json
+from copy import deepcopy
+from .utils import get_chatbot_response, double_check_json_output
+import dotenv
+
+dotenv.load_dotenv()
+
+
+class ClassificationAgent:
+    def __init__(self):
+        self.client = OpenAI(
+            api_key=os.getenv("RUNPOD_TOKEN"), base_url=os.getenv("RUNPOD_CHATBOT_URL")
+        )
+        self.model_name = os.getenv("MODEL_NAME")
+
+    def get_response(self, messages):
+        messages = deepcopy(messages)
+
+        system_prompt = """
+            You are a helpful AI assistant for a coffee shop application.
+            Your task is to determine what agent should handle the user input. You have 3 agents to choose from:
+            1. details_agent: This agent is responsible for answering questions about the coffee shop, like location, delivery places, working hours, details about menue items. Or listing items in the menu items. Or by asking what we have.
+            2. order_taking_agent: This agent is responsible for taking orders from the user. It's responsible to have a conversation with the user about the order untill it's complete.
+            3. recommendation_agent: This agent is responsible for giving recommendations to the user about what to buy. If the user asks for a recommendation, this agent should be used.
+
+            Your output should be in a structured json format like so. each key is a string and each value is a string. Make sure to follow the format exactly:
+            {
+            "chain of thought": "go over each of the agents above and write some your thoughts about what agent is this input relevant to.",
+            "decision": "details_agent" or "order_taking_agent" or "recommendation_agent". Pick one of those. and only write the word.,
+            "message": leave the message empty
+            }
+            """
+
+        input_messages = [{"role": "sysytem", "content": system_prompt}]
+
+        input_messages = input_messages + messages[-3:]
+
+        chatbot_output = get_chatbot_response(self.client, self.model_name, input_messages)
+        chatbot_output = double_check_json_output(self.client, self.model_name, chatbot_output)
+        output = self.postprocess(chatbot_output)
+        return output
+
+    def postprocess(self, output):
+        print("Classification output: ", output)
+        output = json.loads(output)
+
+        dict_output = {
+            "role": "assistant",
+            "content": output["message"],
+            "memory": {
+                "agent": "classification_agent",
+                "classification_decision": output["decision"],
+            },
+        }
+        return dict_output
---
+++ backend/api/development_code.py
@@ -0,0 +1,64 @@
+from agents import (
+    GuardAgent,
+    ClassificationAgent,
+    DetailsAgent,
+    RecommendationAgent,
+    OrderTakingAgent,
+    AgentProtocol,
+)
+from typing import Dict
+import os
+import pathlib
+
+folder_path = pathlib.Path(__file__).parent.resolve()
+
+
+def main():
+    guar_agnet = GuardAgent()
+    clssification_agent = ClassificationAgent()
+    
+    recommendation_agent = RecommendationAgent(
+                                os.path.join(folder_path, "recommendation_objects/apriori_recommendations.json"),
+                                os.path.join(folder_path, "recommendation_objects/popularity_recommendation.csv" ),
+                             )
+    
+    agent_dict : Dict[str, AgentProtocol] =  {
+        "details_agent": DetailsAgent(),
+        "recommendation_agent":  recommendation_agent,
+        "order_taking_agent": OrderTakingAgent( recommendation_agent)
+    }
+    
+    
+    messages = []
+
+    while True:
+        os.system("cls" if os.name == "nt" else "clear")
+        print("\n\n Print Messsage ........")
+        for message in messages:
+            print(f"{message['role']}: {message['content']}")
+
+        # Get user input
+        prompt = input("User: ")
+        messages.append({"role": "user", "content": prompt})
+
+        # Get response from guard agent
+        guar_agnet_response = guar_agnet.get_response(messages)
+        print(f"Guard Agent: {guar_agnet_response['content']}")
+        if guar_agnet_response["memory"]["guard_decision"] == "not allowed":
+            messages.append(guar_agnet_response)
+            continue
+
+        # Get Clarification Agent's response
+        classification_agent_response = clssification_agent.get_response(messages)
+
+        chosen_agent = classification_agent_response["memory"]["classification_decision"]
+
+        # Get response from chosen agent
+        agent = agent_dict[chosen_agent]
+        response = agent.get_response(messages)
+        
+        messages.append(response)
+
+
+if __name__ == "__main__":
+    main()
